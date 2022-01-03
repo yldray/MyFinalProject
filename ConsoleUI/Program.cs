7ec6e662407c8c1d2e359de7cedea91308469f30
Yıldıray TAŞBAŞ <yildirayt@idealdata.com.tr>
2022-01-03 15:13:41 +0300

    Dto yapısı

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,20 +11,36 @@ namespace ConsoleUI
 {
     internal class Program
     {
-
         //SOLID
         //Open Closed Principle
         // Yaptığın yazılıma yeni bir özellik ekliyorsan mevcut kodlara dokunamazsın.
-
         static void Main(string[] args)
+        {
+            //Data Transformation Object
+             ProductTest();
+            //IoC
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var categor in categoryManager.GetAll())
+            {
+                Console.WriteLine(categor.CategoryName);
+
+            }
+        }
+
+        private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product  in productManager.GetByUnitProice(2,300))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "/"+product.CategoryName);
             }
-            
         }
     }
 }
